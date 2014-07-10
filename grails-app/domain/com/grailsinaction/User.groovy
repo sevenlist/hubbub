7a3037c2c4d0d2ed793d29ea434179941699d539
sevenlist <sevenlist@web.de>
2014-07-10 08:27:37 +0200

    Added User domain class and integration test.

---
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -0,0 +1,15 @@
+package com.grailsinaction
+
+class User {
+
+    String loginId
+    String password
+    String homepage
+    Date dateCreated
+
+    static constraints = {
+        loginId size: 3..20, unique: true, nullable: false
+        password size: 6..8, nullable: false
+        homepage url: true, nullable: false
+    }
+}
