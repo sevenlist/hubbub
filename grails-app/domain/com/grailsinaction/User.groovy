9a6e4b2c1607c8786b6d71f5cb6ec6776630845e
sevenlist <sevenlist@web.de>
2014-07-10 08:47:45 +0200

    Added domain class Profile and its relationship with User.

--- grails-app/domain/com/grailsinaction/User.groovy
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -4,12 +4,14 @@ class User {
 
     String loginId
     String password
-    String homepage
     Date dateCreated
+    static hasOne = [profile: Profile]
 
     static constraints = {
         loginId size: 3..20, unique: true, nullable: false
-        password size: 6..8, nullable: false
-        homepage url: true, nullable: false
+        password size: 6..8, nullable: false, validator: { passwd, user ->
+            passwd != user.loginId
+        }
+        profile nullable: true
     }
 }
--- test/integration/com/grailsinaction/UserIntegrationSpec.groovy
+++ test/integration/com/grailsinaction/UserIntegrationSpec.groovy
@@ -6,7 +6,7 @@ class UserIntegrationSpec extends Specification {
 
     def "Saving our first user to the database"() {
         given: "A brand new user"
-        def joe = new User(loginId: 'joe', password: 'secret', homepage: 'http://www.grailsinaction.com')
+        def joe = new User(loginId: 'joe', password: 'secret')
 
         when: "the user is saved"
         joe.save()
@@ -19,7 +19,7 @@ class UserIntegrationSpec extends Specification {
 
     def "Updating a saved user changes its properties"() {
         given: "An existing user"
-        def existingUser = new User(loginId: 'joe', password: 'secret', homepage: 'http://www.grailsinaction.com')
+        def existingUser = new User(loginId: 'joe', password: 'secret')
         existingUser.save(failOnError: true)
 
         when: "A property is changed"
@@ -33,7 +33,7 @@ class UserIntegrationSpec extends Specification {
 
     def "Deleting an existing user removes it from the database"() {
         given: "An existing user"
-        def user = new User(loginId: 'joe', password: 'secret', homepage: 'http://www.grailsinaction.com')
+        def user = new User(loginId: 'joe', password: 'secret')
         user.save(failOnError: true)
 
         when: "The user is deleted"
@@ -46,7 +46,7 @@ class UserIntegrationSpec extends Specification {
 
     def "Saving a user with invalid properties causes an error"() {
         given: "A user which fails several field validations"
-        def user = new User(loginId: 'joe', password: 'tiny', homepage: 'not-a-url')
+        def user = new User(loginId: 'joe', password: 'tiny')
 
         when: "The user is validated"
         user.validate()
@@ -55,20 +55,17 @@ class UserIntegrationSpec extends Specification {
         user.hasErrors()
         "size.toosmall" == user.errors.getFieldError("password").code
         "tiny" == user.errors.getFieldError("password").rejectedValue
-        "url.invalid" == user.errors.getFieldError("homepage").code
-        "not-a-url" == user.errors.getFieldError("homepage").rejectedValue
         !user.errors.getFieldError("loginId")
     }
 
     def "Recovering from a failed save by fixing invalid properties"() {
         given: "A user that has invalid properties"
-        def chuck = new User(loginId: 'chuck', password: 'tiny', homepage: 'not-a-url')
+        def chuck = new User(loginId: 'chuck', password: 'tiny')
         assert chuck.save() == null
         assert chuck.hasErrors()
 
         when: "We fix the invalid properties"
         chuck.password = "fistfist"
-        chuck.homepage = "http://www.chucknorrisfacts.com"
         chuck.validate()
 
         then: "The user saves and validates fine"
